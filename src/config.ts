63ed3b923504528a551cd2ec0f173e498a16b01e
litsynp <nocte55is@gmail.com>
2024-02-14 12:48:21 +0000

    fix: env field name

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,7 @@ export const PND_API_BASE_URL =
   process.env.PND_API_BASE_URL || 'http://localhost:8080'
 
 export const KAKAO_REDIRECT_URI =
-  process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI ||
-  'http://localhost:3000/auth/callback/kakao'
+  process.env.KAKAO_REDIRECT_URI || 'http://localhost:3000/auth/callback/kakao'
 export const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY || ''
 
 export const KAKAO_JS_SDK_VERSION =
